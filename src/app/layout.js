0ccee9e0b352bef90ee14eb25e2c2c1ce5018e6d
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-05-17 02:42:56 -0900

    gtag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,12 +26,30 @@ export default function RootLayout({ children }) {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CEK68M8B3N"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-CEK68M8B3N');
+              `,
+          }}
+        />
+        <meta
+          name="google-site-verification"
+          content="pyLPKww96DsegbH5S1v-qnB35cgrNYfF3G1JKn3Ua-g"
+        />
       </head>
       <body className={inter.className}>
-  
         {children}
-   
-      <LiveChat/>
+
+        <LiveChat />
       </body>
     </html>
   );
